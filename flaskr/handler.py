826bc6e752a0e1bb33dfce5e99a05c0b045ec237
SHREYA VENKATESH PRABHU <shreyaprabhu@SHREYAs-MacBook-Pro.local>
2021-09-02 16:25:42 -0700

    Add profanity checking on search queries

--- flaskr/handler.py
+++ flaskr/handler.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 from spellcorrection import correct_misspelled_words
+from profanity_checker import profanity_check
 from keyword_extractor import rake_impl
 from youtube_api import youtube_api_call
 
@@ -8,16 +10,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 @app.route('/search')
 def search():
     search_query = request.args['search_query']
     search_query = correct_misspelled_words(search_query)
-    keywords = rake_impl(search_query)
-    print(keywords)
-    youtube_response = youtube_api_call(dict(keywords))
-    return youtube_response
+    if(profanity_check(search_query)):
+        raise Exception("Foul language used")
+    else:
+        keywords = rake_impl(search_query)
+        print(keywords)
+        youtube_response = youtube_api_call(dict(keywords))
+        return youtube_response
 
 if __name__ == '__main__':
    app.run()
---
+++ flaskr/profanity_checker.py
@@ -0,0 +1,12 @@
+from profanity_filter import ProfanityFilter
+
+def profanity_check(search_query):
+    pf = ProfanityFilter(languages=['en'])
+    pf.censor_whole_words = False
+    search_query = pf.censor(search_query)
+    print("censored")
+    print(search_query)
+    if "*" in search_query:
+        return True
+    else:
+        return False
